cb973640768511323a9a4b3dbc7efa08b04839c0
root <ryanws@foxmail.com>
2017-03-10 03:01:12 +0800

    sitemap.xml

---
+++ Sitemap/Plugin.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Google Sitemap 生成器
+ * 
+ * @package Sitemap
+ * @author 吴松
+ * @version 1.0.0
+ * @link https://www.bayun.org
+ *
+ */
+class Sitemap_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate()
+    {
+		Helper::addRoute('sitemap', '/sitemap.xml', 'Sitemap_Action', 'action');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate()
+	{
+		Helper::removeRoute('sitemap');
+	}
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+}
---
+++ Sitemap/README.md
@@ -0,0 +1,7 @@
+这款插件通过寒泥Hanny插件修改得来，支持目前最新版本。
+
+[下载地址](https://cdn.bayun.org/software/Sitemap.rar)
+
+上传插件到Typecho安装目录下/usr/plugins，插件文件夹请务必保存为Sitemap（别改就可以）。上传完成在后台启用即可，访问你博客地址/sitemap.xml就可以查看到具体效果。
+
+![](https://cdn.bayun.org/image/typecho-sitemap-one.jpg)
